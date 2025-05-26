35739fdac406f077380ba5dc29c1e123b93866de
Katari <143288488+Katari-8055@users.noreply.github.com>
2025-05-26 12:52:46 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //placeing Order
 export const placeOrder = async (req,res) => {
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://grosify-frontend.onrender.com/";
 
     try {
         const newOrder = new orderModel({
