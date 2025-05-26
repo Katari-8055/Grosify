fe61a22ea7556675ca8bb713853e5d2b391403ea
Katari <143288488+Katari-8055@users.noreply.github.com>
2025-05-26 12:24:02 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const [food_list, setfood_list] = useState([]);
 
-  const url = "http://localhost:4000";
+  const url = "https://grosify-backend.onrender.com";
   const [token, setToken] = useState("");
 
   const fetchFoodList = async () => {
